2db3e308d2773f33c783c93bf23564c5f4a861c4
William Dawn <wcdawn@ncsu.edu>
2017-02-26 22:01:27 -0500

    Endian switch, reading 2D. Need to write 2D record to terminal/ascii.

--- isotxs_io.f90
+++ isotxs_io.f90
@@ -2,7 +2,7 @@ program isotxsio
 IMPLICIT NONE
 
 ! General control variables
-integer :: i
+integer :: i, j
 integer :: ifl, iout, ios
 character(80) :: fname, fname_out
 logical :: lfixstr
@@ -11,9 +11,14 @@ logical :: lfixstr
 character(8) :: hname
 character(8),dimension(2) :: huse
 integer :: ivers
-! FILE CONTROL (1D RECORD)
-integer :: ngroup=0, niso=0, maxup=0, maxdn=0, maxord=0, ichist=0, nscmax=0, nsblok=0
-character(8),dimension(8) :: dummy=''
+! FILE CONTROL   (1D RECORD)
+integer :: ngroup, niso, maxup, maxdn, maxord, ichist, nscmax, nsblok
+! FILE DATA   (2D RECORD)
+character(8),dimension(12) :: hsetid
+character(8),dimension(:),allocatable :: hisonm
+real(4),dimension(:),allocatable :: chi, vel, emax
+real(4) :: emin
+integer,dimension(:),allocatable :: loca
 
 ! Formats
 ! CHARACTER
@@ -25,7 +30,7 @@ character(8),dimension(8) :: dummy=''
            /, a,  ' HNAME         HOLLERITH FILE NAME - ISOTXS', &
            /, a,  ' HUSE(1)       HOLLERITH USER IDENTIFICATION', &
            /, a,  ' HUSE(2)       HOLLERITH USER IDENTIFICATION', &
-           /, i0, ' IVERS         FILE VERSION NUMBER')
+           /, i8, ' IVERS         FILE VERSION NUMBER')
 901 format(/, 'FILE CONTROL   (1D RECORD)', &
            /, i6, ' NGROUP        NUMBER OF ENERGY GROUPS IN FILE', &
            /, i6, ' NISO          NUMBER OF ISOTOPES IN FILE', &
@@ -46,27 +51,31 @@ lfixstr = .false.
 ! OPEN FILES
 !------------------------------------------------------------------------------!
 ! ISOTXS in
+! default open with little_endian. May be closed and reopened later
 open(ifl, file = fname, status = 'old', form = 'unformatted', action = 'read', iostat = ios)
-if (ios .ne. 0) then
-	write(*,'(a,i2,a,a)') 'FATAL -- error opening unit -- ', ifl, ' -- ', fname
-	stop
-endif
+call checkopen(ifl,fname,ios)
 
 ! ASCII out
 ! open(unit = iout, file = fname_out, status = 'replace', action = 'write', iostat = ios)
-! if (ios .ne. 0) then
-	! write(*,'(a,i2,a,a)') 'FATAL -- error opening unit -- ', iout, ' -- ', fname_out
-	! stop
-! endif
+call checkopen(iout,fname_out,ios)
 
 !------------------------------------------------------------------------------!
 ! READ FILE IDENTIFICATION
 !------------------------------------------------------------------------------!
 read(ifl) hname, (huse(i), i = 1,2), ivers
+if ((ivers .lt. 0) .or. (ivers .gt. 20)) then
+	! need big-endian open
+	write(*,101) 'closing ISOTXS and opening with BIG_ENDIAN'
+	close(ifl)
+	open(ifl, file = fname, status = 'old', form = 'unformatted', action = 'read', convert = 'BIG_ENDIAN', iostat = ios)
+	read(ifl) hname, (huse(i), i = 1,2), ivers	
+endif
+	
 
 ! Check if characters need fixing
 if (hname .eq. 'TOSI  SX') then
 	lfixstr = .true.
+	write(*,101) 'fix_str = true'
 endif
 ! fix characters if necessary
 if (lfixstr) then
@@ -84,20 +93,33 @@ if (hname .ne. 'ISOTXS  ') then
 endif
 
 !------------------------------------------------------------------------------!
-! READ FILE CONTROL (1D RECORD)
+! READ FILE CONTROL   (1D RECORD)
 !------------------------------------------------------------------------------!
 read(ifl,iostat = ios) ngroup, niso, maxup, maxdn, maxord, ichist, nscmax, nsblok
 write(*,901) ngroup, niso, maxup, maxdn, maxord, ichist, nscmax, nsblok
 if (ios .ne. 0) then
-	write(*,101) 'FATAL -- error reading FILE CONTROL (1D RECORD)'
-	write(*,'(a,i6)') 'return code', ios
+	write(*,101) 'FATAL -- error reading FILE CONTROL   (1D RECORD)'
+	write(*,'(a,i6)') 'return code = ', ios
 	stop
 endif
 
+write(*,101) 'allocating memory'
+allocate(hisonm(niso))
+allocate(chi(ngroup))
+allocate(vel(ngroup))
+allocate(emax(ngroup))
+allocate(loca(niso))
 
-! read (ifl,iostat=ios) ngroup,niso,maxup,maxdn,maxord,ichist,nscmax,nsblok
-! write (*,901) ngroup,niso,maxup,maxdn,maxord,ichist,nscmax,nsblok
-
+!------------------------------------------------------------------------------!
+! READ FILE DATA   (2D RECORD)
+!------------------------------------------------------------------------------!
+if (ichist .eq. 1) then
+	read(ifl,iostat = ios) (hsetid(i), i = 1,12), (hisonm(i), i = 1,niso), (chi(j), j = 1,ngroup), (vel(j), j = 1,ngroup),&
+	                       (emax(j), j = 1,ngroup), emin, (loca(i), i = 1,niso)
+else
+	read(ifl,iostat = ios) (hsetid(i), i = 1,12), (hisonm(i), i = 1,niso), (vel(j), j = 1,ngroup), &
+	                       (emax(j), j = 1,ngroup), emin, (loca(i), i = 1,niso)
+endif
 
 
 close(ifl)
@@ -116,3 +138,14 @@ str(6:6)=str0(7:7)
 str(7:7)=str0(6:6) 
 str(8:8)=str0(5:5)
 endsubroutine fixstr
+
+subroutine checkopen(ifl,fname,ios)
+IMPLICIT NONE
+integer,intent(in) :: ifl, ios
+character(80),intent(in) :: fname
+if (ios .ne. 0) then
+	write(*,'(a,i2,a,a)') 'FATAL -- error opening unit -- ', ifl, ' -- ', fname
+	write(*,'(a,i3)') 'ios = ', ios
+	stop
+endif
+endsubroutine checkopen
